b73ee89fe5ad6c1079a24c86a69cd5938011cff1
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-18 14:36:21 +0900

    正規表現を用いたバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, :family_name, :first_name, :kana_family_name,
             :kana_first_name, :birthday, presence: true
+
+  validates_format_of :family_name, :first_name, with: /\A[ぁ-んァ-ン一-龥]+\z/
+
+  validates_format_of :kana_family_name, :kana_first_name, with: /\A[ァ-ヶー－]+\z/
+
+  password_vali = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: password_vali
 end
