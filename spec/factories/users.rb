3cbf82ba7fefb4d8320c97f94c468473030a66f7
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-21 22:37:52 +0900

    テストコードとFactoryBotの設定

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  gimei = Gimei.name
+  family_n = gimei.last
+  first_n = gimei.first
+
+  factory :user do
+    nickname {Faker::Internet.username}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    family_name {family_n.kanji}
+    first_name {first_n.kanji}
+    kana_family_name {family_n.katakana}
+    kana_first_name {first_n.katakana}
+    birthday {Faker::Date.between(from: '1930-01-01',to: '2015-12-31')}
+  end
+end
