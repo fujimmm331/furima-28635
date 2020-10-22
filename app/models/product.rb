430aba331469d97b70ef428bc75bada977c73dba
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-22 18:51:39 +0900

    アクティブハッシュのモデル作成と設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :delivery_charge
+  belongs_to_active_hash :delivery_source
+  belongs_to_active_hash :days_to_ship
 end
