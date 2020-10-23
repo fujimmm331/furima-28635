a917626b9a7e7ef433f6ea0b12310acee8d4aac4
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-23 17:07:00 +0900

    コントローラーの設定とビューにcreateアクションのプレフィックス設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,20 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+    
+  end
+
+
+  private
+  def product_params
+    params.require(:product).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_source_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
 end
