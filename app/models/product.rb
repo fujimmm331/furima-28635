63e94adaca5359ddfe10a33bcbafdb3547f089fa
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-24 08:51:51 +0900

    バリデーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,13 @@ class Product < ApplicationRecord
   belongs_to_active_hash :delivery_charge
   belongs_to_active_hash :delivery_source
   belongs_to_active_hash :days_to_ship
+
+  validates :image, :name, :explanation, :category_id, :condition_id,
+            :delivery_charge_id, :delivery_source_id, :days_to_ship_id, 
+            :price, :user, presence: true
+
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message:"is from 300 yen to 9999999 yen, half-width only"}
+
+  validates :category_id, :condition_id, :delivery_charge_id, :delivery_source_id, :days_to_ship_id, numericality: { other_than: 1 , message:"-- is can't choose"}
+
 end
