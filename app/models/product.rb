ad317e054bb1164e77a632de91dc1d3f3fa3372c
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-23 17:05:42 +0900

    アソシエーションの設定

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,8 @@
 class Product < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+  belongs_to :user
+
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
   belongs_to_active_hash :delivery_charge
