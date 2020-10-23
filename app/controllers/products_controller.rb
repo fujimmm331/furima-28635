211e6d0784da96deb409587cf0373a9c1ada12c3
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-23 17:27:22 +0900

    ログインしているユーザーのみ出品ページへ遷移

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @products = Product.all.order('created_at DESC')
   end
@@ -22,4 +24,5 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_source_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
   end
+
 end
