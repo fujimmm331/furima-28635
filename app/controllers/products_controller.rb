a4a8d313eed7f2eecb3b4ba2f41e6f339ef9ecf7
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-10-25 18:09:55 +0900

    パスを直接入力されてもアクセスできないように設定

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :create]
+  before_action :protect_user, only: [:edit, :update,]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -39,5 +40,11 @@ class ProductsController < ApplicationController
 
   def product_params
     params.require(:product).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_charge_id, :delivery_source_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def protect_user
+    if Product.find(params[:id]).user.id != current_user.id
+      redirect_to root_path
+    end
   end
 end
