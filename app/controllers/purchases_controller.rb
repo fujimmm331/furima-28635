93d0ea46e3bb969f0487a1fec64d46d0bb8b8bd7
nyappy15ch <69615045+nyappy15ch@users.noreply.github.com>
2020-11-03 20:12:48 +0900

    複数テーブルにデータを保存とバリデーションの実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,22 @@
 class PurchasesController < ApplicationController
   def new
-    @purchase = Purchase.new
+    @purchase_delivery_information = PurchaseDeliveryInformation.new
     @product = Product.find(params[:product_id])
   end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @purchase_delivery_information = PurchaseDeliveryInformation.new(purchase_params)
+    if @purchase_delivery_information.valid?
+      @purchase_delivery_information.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def purchase_params
+    params.require(:purchase_delivery_information).permit(:postal_code, :delivery_source_id, :municipality, :address, :building, :telephone_number, :purchase).merge(user: current_user.id, product: @product.id)
+ end
 end
